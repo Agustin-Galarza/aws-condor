04412aa5a019134499fdfee77105039175c5f38c
Agustin Galarza <agalarza@itba.edu.ar>
2023-12-31 01:25:59 -0300

    Fixed npm build script

--- scripts/npm_build.sh
+++ scripts/npm_build.sh
@@ -1,11 +1,11 @@
 cd resources/frontend/condor
 npm install > /dev/null 2>&1
-if $? -ne 0; then
+if [ $? -ne 0 ]; then
     exit 1;
 fi
 
 npm run build > /dev/null 2>&1
-if $? -ne 0; then
+if [ $? -ne 0 ]; then
     exit 2;
 fi
 
