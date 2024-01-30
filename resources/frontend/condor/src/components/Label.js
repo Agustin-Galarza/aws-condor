1c52da0bcfe962bcf55d9b4ad7baa213ad573035
aBenve <abenvenuto@itba.edu.ar>
2024-01-29 21:21:32 -0300

    remove unused js from frontend

--- resources/frontend/condor/src/components/Label.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-function Label({ email, onChange, id, type, label }) {
-    return (_jsxs("label", { htmlFor: "email", className: "flex flex-col gap-2", children: [_jsx("span", { className: "text-zinc-300 text-sm font-bold", children: label }), _jsx("input", { value: email, onChange: (e) => onChange(e.target.value), className: "appearance-none bg-transparent border-zinc-600 border-[1px] rounded w-full py-2 px-3 text-gray-300 leading-tight focus:outline-none focus:shadow-outline", id: id, type: type })] }));
-}
-export default Label;
--- resources/frontend/condor/src/routes/404.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { useNavigate } from "react-router-dom";
-function NotFound() {
-    const navigate = useNavigate();
-    return (_jsxs("div", { className: "flex flex-col items-center justify-center min-h-screen gap-8", children: [_jsxs("div", { className: "flex flex-col gap-2 items-center", children: [_jsx("h1", { className: "text-[7rem] leading-[8rem] font-bold", children: "404" }), _jsx("span", { className: "text-xl", children: "Page not found" })] }), _jsx("button", { onClick: () => navigate("/"), className: "px-3 py-2 rounded  bg-red-500 font-extrabold", children: "Go Home" })] }));
-}
-export default NotFound;
