31a044d55c1ed13e7b35e372b9b2bad52c14a8f3
aBenve <abenvenuto@itba.edu.ar>
2024-02-06 16:54:43 -0300

    add region to sns helper

--- scripts/aws_helpers/snsHelper.mjs
+++ scripts/aws_helpers/snsHelper.mjs
@@ -1,92 +1,94 @@
-import { SNSClient } from '@aws-sdk/client-sns';
+import { SNSClient } from "@aws-sdk/client-sns";
 import {
-	CreateTopicCommand,
-	DeleteTopicCommand,
-	SubscribeCommand,
-	UnsubscribeCommand,
-	PublishCommand,
-} from '@aws-sdk/client-sns';
+  CreateTopicCommand,
+  DeleteTopicCommand,
+  SubscribeCommand,
+  UnsubscribeCommand,
+  PublishCommand,
+} from "@aws-sdk/client-sns";
 
 // The AWS Region can be provided here using the `region` property. If you leave it blank
 // the SDK will default to the region set in your AWS config.
-const snsClient = new SNSClient({});
+const snsClient = new SNSClient({
+  region: "us-east-1",
+});
 
-export const createTopic = async topicName => {
-	try {
-		const data = await snsClient.send(
-			new CreateTopicCommand({
-				Name: topicName,
-			})
-		);
-		console.log('Topic ARN is ' + data.TopicArn);
-		return data.TopicArn;
-	} catch (err) {
-		console.error(err, err.stack);
-		return null;
-	}
+export const createTopic = async (topicName) => {
+  try {
+    const data = await snsClient.send(
+      new CreateTopicCommand({
+        Name: topicName,
+      })
+    );
+    console.log("Topic ARN is " + data.TopicArn);
+    return data.TopicArn;
+  } catch (err) {
+    console.error(err, err.stack);
+    return null;
+  }
 };
 
-export const deleteTopic = async topicArn => {
-	try {
-		const data = await snsClient.send(
-			new DeleteTopicCommand({
-				TopicArn: topicArn,
-			})
-		);
-		console.log('Topic deleted');
-		return true;
-	} catch (err) {
-		console.error(err, err.stack);
-		return false;
-	}
+export const deleteTopic = async (topicArn) => {
+  try {
+    const data = await snsClient.send(
+      new DeleteTopicCommand({
+        TopicArn: topicArn,
+      })
+    );
+    console.log("Topic deleted");
+    return true;
+  } catch (err) {
+    console.error(err, err.stack);
+    return false;
+  }
 };
 
 export const publish = async (topicArn, subject, message) => {
-	try {
-		const data = await snsClient.send(
-			new PublishCommand({
-				TopicArn: topicArn,
-				Message: message,
-				Subject: subject,
-			})
-		);
-		console.log('Message sent');
-		return true;
-	} catch (err) {
-		console.error(err, err.stack);
-		return false;
-	}
+  try {
+    const data = await snsClient.send(
+      new PublishCommand({
+        TopicArn: topicArn,
+        Message: message,
+        Subject: subject,
+      })
+    );
+    console.log("Message sent");
+    return true;
+  } catch (err) {
+    console.error(err, err.stack);
+    return false;
+  }
 };
 
-export const subscribe = async (topicArn, endpoint, protocol = 'email') => {
-	try {
-		const data = await snsClient.send(
-			new SubscribeCommand({
-				TopicArn: topicArn,
-				Protocol: protocol,
-				Endpoint: endpoint,
-				ReturnSubscriptionArn: true,
-			})
-		);
-		console.log('Subscription ARN is ' + data.SubscriptionArn);
-		return data.SubscriptionArn;
-	} catch (err) {
-		console.error(err, err.stack);
-		return null;
-	}
+export const subscribe = async (topicArn, endpoint, protocol = "email") => {
+  try {
+    const data = await snsClient.send(
+      new SubscribeCommand({
+        TopicArn: topicArn,
+        Protocol: protocol,
+        Endpoint: endpoint,
+        ReturnSubscriptionArn: true,
+      })
+    );
+    console.log("Subscription ARN is " + data.SubscriptionArn);
+    return data.SubscriptionArn;
+  } catch (err) {
+    console.error(err, err.stack);
+    return null;
+  }
 };
 
-export const deleteSubcription = async subscriptionArn => {
-	try {
-		const data = await snsClient.send(
-			new UnsubscribeCommand({
-				SubscriptionArn: subscriptionArn,
-			})
-		);
-		console.log('Subscription deleted');
-		return true;
-	} catch (err) {
-		console.error(err, err.stack);
-		return false;
-	}
+export const deleteSubcription = async (subscriptionArn) => {
+  try {
+    const data = await snsClient.send(
+      new UnsubscribeCommand({
+        SubscriptionArn: subscriptionArn,
+      })
+    );
+    console.log("Subscription deleted");
+    return true;
+  } catch (err) {
+    console.error(err, err.stack);
+    return false;
+  }
 };
